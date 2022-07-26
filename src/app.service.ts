cef42743fcd605e812a98373d06fa6a47749e202
Jerome Le Baron <jerome.lebaron@fabernovel.com>
2022-07-26 16:38:27 +0200

    [LOG] Add logs to controller and service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PinoLogger } from 'nestjs-pino';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly logger: PinoLogger) {
+    logger.setContext(AppService.name);
+  }
+
   getHello(): string {
+    this.logger.info('Get Hello from service');
     return 'Hello World!';
   }
 }
