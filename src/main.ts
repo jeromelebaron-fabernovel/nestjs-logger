f61404d374e54ec707af82de3fd8a11337c395cb
Jerome Le Baron <jerome.lebaron@fabernovel.com>
2022-07-26 11:05:12 +0200

    [LOG] Configure winston as global logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+      transports: [new winston.transports.Console()],
+    }),
+  });
   await app.listen(3000);
 }
 bootstrap();
