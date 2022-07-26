ddb77b4cbfa849c8a32318aaa33f237306a1e9a0
Jerome Le Baron <jerome.lebaron@fabernovel.com>
2022-07-26 15:37:31 +0200

    [LOG] Configure pino as global logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
+        genReqId: () => randomUUID,
+        autoLogging: false,
+        messageKey: 'message',
+        redact: {
+          paths: ['password'],
+          censor: '*********',
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
