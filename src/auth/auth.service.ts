a8a6d6c3e1eb28bc5c713e408d8220466648b058
Jerome Le Baron <jerome.lebaron@fabernovel.com>
2022-07-27 16:59:44 +0200

    [LOG] Add log on user login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PinoLogger } from 'nestjs-pino';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly logger: PinoLogger,
+  ) {
+    this.logger.setContext(AuthService.name);
+  }
 
   async login(login: string, password: string): Promise<any> {
+    this.logger.info(`Trying to log ${login}`);
     const foundUser = await this.usersService.findOne(login);
     if (foundUser && password === foundUser.password) {
       const { password, ...result } = foundUser;
